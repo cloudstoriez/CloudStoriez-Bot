716fecb025dc151b4e35e29a8b2027b0311feb0b
cloudstoriez <cloudstoriez@gmail.com>
2026-09-21 17:05:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 import os
+from threading import Thread
+from flask import Flask
 import discord
 from discord.ext import commands
 
+# Μικρός web server για το Render
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is running!"
+
+def run_web():
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
+
+# Εκκίνηση web server σε ξεχωριστό thread
+Thread(target=run_web).start()
+
+# Discord Bot
 intents = discord.Intents.default()
 intents.message_content = True
 
@@ -9,7 +26,7 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f'Το bot {bot.user} είναι online 24/7!')
+    print(f'Το bot {bot.user} είναι online!')
 
 @bot.command()
 async def ping(ctx):
